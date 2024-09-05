413f6842d799900a2e5c2d9f9e288a4e0d1f0b1b
BenVingarten <bv080899@gmail.com>
2024-09-05 12:35:30 +0300

    changed to lower case

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>React Tutorial</h1>
+        <h1>teact tutorial</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
